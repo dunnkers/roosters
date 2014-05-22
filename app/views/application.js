5543c73881ed4ed2c661846c87928a1de402a4cd
Dunnkers <dunnkers@hotmail.com>
2014-05-22 19:57:40 +0200

    Style different datasets and append the clear input fields' close-icon

--- app/views/application.js
+++ app/views/application.js
@@ -12,11 +12,18 @@ export default Ember.View.extend({
 		}, {
 			name: 'teachers-search',
 			displayKey: 'unique',
-			source: Ember.teacherEngine.ttAdapter()
+			source: Ember.teacherEngine.ttAdapter(),
+			templates: {
+				header: '<hr>'
+			}
 		}).on('typeahead:selected', function (event, item) {
 			self.get('controller').set('searchValue', item.unique);
+			self.controller.send('search');
 		}).on('typeahead:autocompleted', function (event, item) {
 			self.get('controller').set('searchValue', item.unique);
 		}).focus();
+
+		// move the close icon next to typeahead
+		$('#bloodhound .twitter-typeahead').append($('#bloodhound .close-icon'));
 	}
 });
