58ec0d05bfb903754149c2249077ecca5d290c3c
dunnkers <dunnkers@hotmail.com>
2014-11-19 01:27:55 +0100

    Sibling grouping.

--- app/controllers/schedule-view.js
+++ app/controllers/schedule-view.js
@@ -12,6 +12,35 @@ export default Ember.ArrayController.extend({
         // group cells
         var days = groupBy(row.get('content'), 'day');
 
+        // group siblings
+        days = days.map(function (lesson) {
+            var siblings = groupBy(lesson.get('content'), 'cluster.id');
+
+            siblings = siblings.map(function (sibling) {
+              var content = Ember.Object.create({
+                subject: sibling.get('content.firstObject.subject'),
+                cluster: sibling.get('content.firstObject.cluster'),
+                groups: [],
+                rooms: [],
+                teachers: []
+              });
+
+              sibling.get('content').forEach(function (lesson) {
+                if (lesson.get('group')) content.groups.push(lesson.get('group'));
+                if (lesson.get('room')) content.rooms.push(lesson.get('room'));
+                if (lesson.get('teacher')) content.teachers.push(lesson.get('teacher'));
+              });
+
+              return content;
+            });
+
+            return Ember.ArrayController.create({
+              model: siblings,
+              sortProperties: [ 'group' ],
+              sortAscending: true
+            });
+        });
+
         days.insertAt(0, Ember.Object.create({
           group: -1,
           header: true,
