ee9da959fdc4c633094c3efa7059f6ce070cf1c6
dunnkers <dunnkers@hotmail.com>
2014-12-06 18:03:28 +0100

    Refactor cell display to use recursion for better abstraction.

--- app/components/cell-display.js
+++ app/components/cell-display.js
@@ -1,5 +1,4 @@
 import Ember from 'ember';
-import groupByProperties from '../utils/group-by-properties';
 
 export default Ember.Component.extend({
   // single
@@ -9,19 +8,5 @@ export default Ember.Component.extend({
 
   lesson: function () {
     return this.get('lessons.firstObject');
-  }.property('lessons'),
-
-  // nested
-  nested: function () {
-    return groupByProperties(this.get('lessons'), [ 'subject', 'cluster.id' ]);
-  }.property('lessons'),
-
-  isNested: function () {
-    return this.get('nested.length') > 1;
-  }.property('nested'),
-
-  // single sibling
-  siblings: function () {
-    return this.get('nested.firstObject');
-  }.property('nested')
+  }.property('lessons')
 });
--- app/components/nested-lesson.js
+++ app/components/nested-lesson.js
@@ -1,11 +1,17 @@
 import Ember from 'ember';
+import groupByProperties from '../utils/group-by-properties';
 
 export default Ember.Component.extend({
-  single: function () {
-    return this.get('lessons.length') === 1;
+  nested: function () {
+    return groupByProperties(this.get('lessons'), [ 'subject', 'cluster.id' ]);
   }.property('lessons'),
 
-  lesson: function () {
-    return this.get('lessons.firstObject');
-  }.property('lessons'),
+  isNested: function () {
+    return this.get('nested.length') > 1;
+  }.property('nested'),
+
+  // single sibling
+  siblings: function () {
+    return this.get('nested.firstObject');
+  }.property('nested')
 });
