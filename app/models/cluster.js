3c1620e7131db09a25c19d417c3b533c046cb4a0
Dunnkers <dunnkers@hotmail.com>
2014-12-16 00:36:40 +0100

    Display clusters in a grade.

--- app/models/cluster.js
+++ app/models/cluster.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
+  grade: DS.belongsTo('grade'),
   students: DS.hasMany('student', { async: true })
 });
--- app/models/grade.js
+++ app/models/grade.js
@@ -1,5 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  groups: DS.hasMany('group', { async: true })
+  groups: DS.hasMany('group', { async: true }),
+  clusters: DS.hasMany('cluster', { async: true })
 });
