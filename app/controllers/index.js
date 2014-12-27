6c4ee31ef548f925d1b3dddffe032d2dfb38c1cc
Dunnkers <dunnkers@hotmail.com>
2014-12-27 19:33:29 +0100

    Also send outlet loaded action from index.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,5 +2,11 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortProperties: [ 'id' ],
-  sortAscending: false
+  sortAscending: false,
+
+  modelChanged: function() {
+    Ember.run.once(this, () => {
+      this.send('outletLoaded');
+    });
+  }.observes('model'),
 });
