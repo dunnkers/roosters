5c157044b0ba98a82ac7e8b1421f0b6b95703cf8
Dunnkers <dunnkers@hotmail.com>
2015-01-03 04:20:27 +0100

    Remove item fetching and bloodhound functionality; fetching on the fly is better.

--- app/components/typeahead-field.js
+++ app/components/typeahead-field.js
@@ -1,6 +1,4 @@
 import Ember from 'ember';
-// JSHint exception
-/* global Bloodhound */
 
 export default Ember.TextField.extend({
   classNames: [ 'typeahead' ],
@@ -9,37 +7,6 @@ export default Ember.TextField.extend({
   // id and display key
   unique: 'id',
   displayKey: 'title',
-
-  contentChanged: function () {
-    Ember.Logger.debug('Feeding BloodHound with data...');
-    var time = new Date();
-
-    this.get('engine').add(this.get('items'));
-
-    Ember.Logger.debug('BloodHound fed -', new Date() - time, 'ms');
-  }.observes('content'),
-
-  items: function () {
-    return (this.get('content') || []).map((item) => {
-      return item.getProperties(this.get('unique'), this.get('displayKey'));
-    });
-  }.property('content'),
-
-  initializeBloodhound: function () {
-    var bloodhound = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace(this.get('displayKey')),
-      queryTokenizer: Bloodhound.tokenizers.whitespace,
-      local: this.get('items')
-    });
-
-    bloodhound.initialize().done(function () {
-      Ember.Logger.debug('Suggestion engine successfully initialized.');
-    }).fail(function () {
-      Ember.Logger.debug('Failed to initialize suggestion engine');
-    });
-
-    this.set('engine', bloodhound);
-  }.on('init'),
 
   // Ember cookbook: Focusing a textfield after it's been inserted
   becomeFocused: function () {
@@ -51,66 +18,17 @@ export default Ember.TextField.extend({
       highlight: true
     }, {
       displayKey: this.get('displayKey'),
-      source: this.get('engine').ttAdapter(),
-      templates: {
-        suggestion: function (suggestion) {
-          var template = '{{#link-to "item" %@ href="/%@"}}%@{{/link-to}}'.fmt(
-            suggestion[this.get('unique')],
-            suggestion[this.get('unique')],
-            suggestion[this.get('displayKey')]
-          );
-
-          var view = Ember.View.create({
-            template: Ember.Handlebars.compile(template)
-          });
-
-          var $elem;
-          Ember.run(function() {
-            $elem = Ember.$('<div>');
-            view.appendTo($elem);
-          });
-
-          return $elem.html();
-        }
-      }
+      source: () => {}
     });
 
     element.on('typeahead:selected', (event, item) => {
       // ensure component value is also set
       this.set('value', item[this.get('displayKey')]);
-
-      // also bubble an action when suggestion is clicked
-      this.sendSuggestion(item);
     });
   }.on('didInsertElement'),
 
-  valChanged: function () {
-    Ember.Logger.debug('check it.', Ember.$('.tt-dataset-0').html());
-  }.observes('value'),
-
-  // bubble up an action
   insertNewline: function () {
-    this.get('engine').get(this.get('value'), (suggestions) => {
-      var suggestion = suggestions.get('firstObject');
-
-      if (suggestion) {
-        // ensure all inputs have the same value
-        this.set('value', suggestion[this.get('displayKey')]);
-        Ember.$('.typeahead').typeahead('val', suggestion[this.get('displayKey')]);
-
-        // close dropdown menu. this must be done after setting the value.
-        Ember.$('.typeahead').typeahead('close');
-
-        // send action
-        this.sendSuggestion(suggestion);
-      }
-    });
-  },
-
-  sendSuggestion: function (suggestion) {
-    var object = this.get('content').findBy(this.get('unique'),
-      suggestion[this.get('unique')]);
-
-    this.sendAction('search', object);
+    // close dropdown menu. this must be done after setting the value.
+    Ember.$('.typeahead').typeahead('close');
   }
 });
--- app/controllers/grades.js
+++ app/controllers/grades.js
@@ -2,11 +2,5 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortProperties: [ 'id' ],
-  sortAscending: false,
-
-  modelChanged: function () {
-    Ember.run.once(this, () => {
-      this.send('outletLoaded');
-    });
-  }.observes('model'),
+  sortAscending: false
 });
