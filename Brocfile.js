ab96a9917211d464476cd15ce6e7effe0b36bfa2
Dunnkers <dunnkers@hotmail.com>
2015-01-05 19:03:10 +0100

    Don't import unused bloodhound.

--- Brocfile.js
+++ Brocfile.js
@@ -8,7 +8,10 @@ var app = new EmberApp({
   }
 });
 
-app.import(app.bowerDirectory + '/typeahead.js/dist/typeahead.bundle.js');
+app.import({
+  development: app.bowerDirectory + '/typeahead.js/dist/typeahead.jquery.js',
+  production: app.bowerDirectory + '/typeahead.js/dist/typeahead.jquery.min.js'
+});
 app.import(app.bowerDirectory + '/moment/locale/nl.js');
 
 // Use `app.import` to add additional libraries to the generated
