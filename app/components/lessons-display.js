990d8f0f95dd44ea857e392cbd76371dcedee4e5
Dunnkers <dunnkers@hotmail.com>
2015-01-06 14:28:39 +0100

    Attach cell tagname 'td' to lesson- and lessons display.

--- app/components/lessons-display.js
+++ app/components/lessons-display.js
@@ -2,16 +2,13 @@ import Ember from 'ember';
 import groupBy from '../utils/group-by';
 
 export default Ember.Component.extend({
+  tagName: 'td',
+
   nested: function () {
     return groupBy(this.get('lessons'), 'subject', 'cluster.id' );
   }.property('lessons'),
 
   isNested: function () {
     return this.get('nested.length') > 1;
-  }.property('nested'),
-
-  // single sibling
-  siblings: function () {
-    return this.get('nested.firstObject');
   }.property('nested')
 });
--- app/components/sibling-lesson.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Component.extend({
-  default: function () {
-    return this.get('siblings.firstObject');
-  }.property('siblings')
-});
